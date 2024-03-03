65e756b832cd1b272339877518668b1403f171a3
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-03-03 21:37:21 +0200

    added task_01.py, task_set.py

--- task_01.py
+++ task_01.py
@@ -9,7 +9,7 @@ array = [[random.randint(10, 99) for i in range(5)] for j in range(5)]
 
 for list in range(len(array)):
     metrics = [str(j) for j in array[list]]
-    print(" ".join(metrics))
+    print("\U0001f606".join(metrics))
 
 print()
 for list in array:
--- task_set.py
+++ task_set.py
@@ -2,8 +2,8 @@ set_0 = {"Comedy", "Drama", "Adventure", "Western", "Horror", "Science Fiction"}
 set_1 = {"Action", "Melodrama", "Western", "Horror", "Mystery", "Comedy"}
 
 print(
-    f"""intersection set : {set_0.intersection(set_1)}
-difference set 0 : {set_0.difference(set_1)}
-difference set 1 : {set_1.difference(set_0)}
-symmetric difference eset : {set_0.symmetric_difference(set_1)}"""
+    f"""intersection set : {set_0.intersection(set_1)} \U0001f607
+difference set 0 : {set_0.difference(set_1)} \U0001f608
+difference set 1 : {set_1.difference(set_0)} \U0001f609
+symmetric difference eset : {set_0.symmetric_difference(set_1)} \U0001f610"""
 )
