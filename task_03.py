a6796bf8f08adda23b19d3519b09446a7fc5cba5
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-03-04 14:51:28 +0200

    added task_03.py, task_04.py

--- task_03.py
+++ task_03.py
@@ -2,8 +2,8 @@ import random
 
 array = [[chr(random.randint(65, 90)) for i in range(5)] for j in range(5)]
 
-for list in range(len(array)):
-    metrics = [str(j) for j in array[list]]
+for index in range(len(array)):
+    metrics = [str(j) for j in array[index]]
     print(" ".join(metrics))
 
 print()
--- task_04.py
+++ task_04.py
@@ -5,14 +5,14 @@ def comparison(num: str):
     sum = 0
     for index in range(len(num)):
         sum += int(num[index])
-    return num if sum % 2 == 0 else " "
+    return num if sum % 2 == 0 else "  "
 
 
 array = [[random.randint(10, 99) for i in range(5)] for j in range(5)]
 
-for list in range(len(array)):
-    metrics = [str(j) for j in array[list]]
-    print("\U0001f604".join(metrics))
+for index in range(len(array)):
+    metrics = [str(j) for j in array[index]]
+    print(" ".join(metrics))
 
 print()
 for list in array:
