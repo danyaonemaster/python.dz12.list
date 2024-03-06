33280aab7989728d85d3a8d3fc31bd2120e6e773
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-03-07 00:01:39 +0200

    added cllas.py

--- cllas.py
+++ cllas.py
@@ -1,13 +1,28 @@
-class if_else:
-    def __init__(self, list, average_or_sum=1):
-        self.letters = [chr(letter) if chr(letter) == "T" else " "
-                        for letter in list]
+class Matrix:
+    def __init__(self, array, arith_averag=0):
+        self.array = array
+        self.arith_averag = arith_averag
 
-        self.values = [str(num) if str(num).__contains__("5") else "  "
-                       for num in list]
+    def get_matrix(self):
+        for list in self.array:
+            print("  ".join([str(num) for num in list]))
 
-        self.average = [str(num) if num > average_or_sum
-                        else "  " for num in list]
+    def get_values(self):
+        for list in self.array:
+            print("  ".join([str(num) if str(num).__contains__("5") else "  "
+                             for num in list]))
 
-        self.evensum = [str(num) if sum(map(int, str(num))) % 2 == 0 else "  "
-                        for num in list]
+    def get_letters(self):
+        for list in self.array:
+            print("  ".join([letter if letter == "T" else " "
+                             for letter in list]))
+
+    def get_average(self):
+        for list in self.array:
+            print("  ".join([str(num) if num > self.arith_averag
+                             else "  " for num in list]))
+
+    def get_even_sum(self):
+        for list in self.array:
+            print("  ".join([str(num) if sum(map(int, str(num))) % 2 == 0 else "  "
+                             for num in list]))
