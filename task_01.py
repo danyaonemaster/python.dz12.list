783e9f6a6ef3387354f3ca8c64d385d47aae40b3
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-03-07 23:24:28 +0200

    added task_01.py, task_02.py

--- task_01.py
+++ task_01.py
@@ -1,12 +1,14 @@
-from cllas import Matrix
+from classes import Matrix
+from helpers import sep
 import random
+import helpers
+
 
 array = [[random.randint(10, 99) for i in range(5)] for j in range(5)]
 
 for list in array:
-    print("\U0001f606"
-          .join([str(j) for j in list]))
+    print("\U0001f606".join([str(j) for j in list]))
 
 metrics = Matrix(array)
-
-metrics.get_values()
+sep()
+metrics.print_values_by_content("5")
--- task_02.py
+++ task_02.py
@@ -1,5 +1,7 @@
 import random
-from cllas import Matrix
+
+import helpers
+from classes import Matrix
 
 array = [[random.randint(10, 99) for i in range(5)] for j in range(5)]
 sum_list = 0
@@ -9,11 +11,9 @@ for list in array:
     len_array += len(list)
     sum_list += sum(list)
 
-
 average_arithmetic = sum_list / len_array
-matrix_average = Matrix(array,average_arithmetic)
+matrix_average = Matrix(array, average_arithmetic)
 
-matrix_average.get_matrix()
-print()
+matrix_average.print_matrix()
+helpers.sep()
 matrix_average.get_average()
-
