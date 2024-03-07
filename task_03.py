e4c736544a8c405f9c7b2a9ec23e3aca307d4e69
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-03-07 23:24:55 +0200

    added task_03.py, task_04.py

--- task_03.py
+++ task_03.py
@@ -1,9 +1,10 @@
-from cllas import Matrix
+import helpers
+from classes import Matrix
 import random
 
 array = [[chr(random.randint(65, 90)) for i in range(5)] for j in range(5)]
 matrix_letter = Matrix(array)
 
-matrix_letter.get_matrix()
-print()
+matrix_letter.print_matrix()
+helpers.sep()
 matrix_letter.get_letters()
--- task_04.py
+++ task_04.py
@@ -1,10 +1,12 @@
 import random
-from cllas import Matrix
+
+import helpers
+from classes import Matrix
 
 array = [[random.randint(10, 99) for i in range(5)] for j in range(5)]
 list_sum = 0
 matrix_even_sum = Matrix(array)
 
-matrix_even_sum.get_matrix()
-print()
+matrix_even_sum.print_matrix()
+helpers.sep()
 matrix_even_sum.get_even_sum()
