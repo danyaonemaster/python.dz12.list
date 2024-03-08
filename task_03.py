c945c80c143d769c2230f4321cb8d44f96453c62
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-03-08 17:14:04 +0200

    added task_03.py, task_04.py

--- task_03.py
+++ task_03.py
@@ -7,4 +7,10 @@ matrix_letter = Matrix(array)
 
 matrix_letter.print_matrix()
 helpers.sep()
+
+for child_list in array:
+    print("  ".join([letter if letter == "T" else " "
+                     for letter in child_list]))
+
+helpers.sep()
 matrix_letter.get_letters()
--- task_04.py
+++ task_04.py
@@ -9,4 +9,10 @@ matrix_even_sum = Matrix(array)
 
 matrix_even_sum.print_matrix()
 helpers.sep()
+
+for child_list in array:
+    print("  ".join([str(num) if sum(map(int, str(num))) % 2 == 0 else "  "
+                     for num in child_list]))
+
+helpers.sep()
 matrix_even_sum.get_even_sum()
